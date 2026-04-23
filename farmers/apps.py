a4f125289cada904cd10cdf949613914f5215e2f
P-Alakara <alakara11728@gmail.com>
2026-04-23 20:35:01 +0300

    create signals to auto-create profiles

--- farmers/apps.py
+++ farmers/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class FarmersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'farmers'
+
+    def ready(self):
+        import farmers.signals
