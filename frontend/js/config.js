93845784d5b054cddaa8b6ddd26897f240652ec7
P-Alakara <alakara11728@gmail.com>
2026-04-24 19:51:11 +0300

    change prod API url

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,6 +5,6 @@ const IS_PRODUCTION = window.location.hostname !== '127.0.0.1'
                    && window.location.hostname !== 'localhost';
 
 const DEV_API_URL  = 'http://127.0.0.1:8000/api';
-const PROD_API_URL = 'https://shamba-api-c9tf.onrender.com';
+const PROD_API_URL = 'https://shamba-api-c9tf.onrender.com/api';
 
 const API_BASE = IS_PRODUCTION ? PROD_API_URL : DEV_API_URL;
